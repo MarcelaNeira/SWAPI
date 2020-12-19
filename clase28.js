d79226c52c465dec81949f8b4e53ce5890dacac2
Marcela Neira <martaymarcela@gmail.com>
2020-12-19 14:41:15 +0100

    Callback hell

--- clase28.js
+++ clase28.js
@@ -2,14 +2,39 @@ const API_URL = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/'
 
 const opts = {crossDomain:true}
-const onPeopleResponse =  function(data){
-    console.log(`Hola soy ${data.name}`)
+
+
+const obtenerPersonaje = function(id, callback){
+    $.get(`${API_URL}${PEOPLE_URL}${id}`,opts, function(data){
+        
+            console.log(`Hola soy ${data.name}`)
+
+            if(callback){
+                callback()
+            }
+    })
 }
 
-const obtenerPersonaje = function(id){
-    $.get(`${API_URL}${PEOPLE_URL}${id}`,opts,onPeopleResponse)
-}
-
+/*
+Esto no nos garantiza que el resultado de la función sea en el mismo orden del que se le llama
 for(i=1;i<=5;i++){
     obtenerPersonaje(i)
-}
+}*/
+
+/*
+obtenerPersonaje(1, obtenerPersonaje(2))
+Esto si lo llamamos de esta manera hará que primero se ejecute la función que esta dentro de los párametros 
+para que esto no suceda y lo que que se llame sea después de ejecutar la primera función debemos definir la 
+función como en el siguiente párrafo*/
+
+obtenerPersonaje(1, function(){
+    obtenerPersonaje(2, function(){
+        obtenerPersonaje(3, function(){
+            obtenerPersonaje(4, function(){
+                obtenerPersonaje(5,function(){
+                    obtenerPersonaje(6)
+                })
+            })
+        })
+    })
+})
