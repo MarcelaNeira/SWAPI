06ab9ba037329a29d7835ba400a2a076deb5a347
Marcela Neira <martaymarcela@gmail.com>
2020-12-19 13:38:23 +0100

    Clarify the code

--- clase28.js
+++ clase28.js
@@ -1,7 +1,9 @@
 const API_URL = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/'
-
-$.get(`${API_URL}${PEOPLE_URL}1`,{crossDomain:true}, function(data){
+const opts = {crossDomain:true}
+const onPeopleResponse =  function(data){
     console.log(`Hola soy ${data.name}`)
-})
+}
+
+$.get(`${API_URL}${PEOPLE_URL}1`,opts,onPeopleResponse)
 
