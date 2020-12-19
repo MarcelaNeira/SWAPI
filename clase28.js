891cfaa40d16da53f2ae0a220c1fa605fda2ba94
Marcela Neira <martaymarcela@gmail.com>
2020-12-19 14:15:36 +0100

    Get the first five characters

--- clase28.js
+++ clase28.js
@@ -1,9 +1,15 @@
 const API_URL = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/'
+
 const opts = {crossDomain:true}
 const onPeopleResponse =  function(data){
     console.log(`Hola soy ${data.name}`)
 }
 
-$.get(`${API_URL}${PEOPLE_URL}1`,opts,onPeopleResponse)
+const obtenerPersonaje = function(id){
+    $.get(`${API_URL}${PEOPLE_URL}${id}`,opts,onPeopleResponse)
+}
 
+for(i=1;i<=5;i++){
+    obtenerPersonaje(i)
+}
