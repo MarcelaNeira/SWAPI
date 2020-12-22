c1c6bc8840844123d3d3d7a16b8cadccab1c9136
Marcela Neira <martaymarcela@gmail.com>
2020-12-22 20:15:36 +0100

    Generating a sequence of numbers

---
+++ SimonDice.js
@@ -0,0 +1,22 @@
+const btnEmpezar = document.getElementById('btnEmpezar')
+const celeste = document.getElementById('celeste')
+const violeta = document.getElementById('violeta')
+const naranja = document.getElementById('naranja')
+const verde = document.getElementById('verde')
+
+class Juego{
+    constructor(){
+        this.inicializar()
+        this.generarSecuencia()
+    }
+    inicializar(){
+        btnEmpezar.classList.add('hide')
+    }
+    generarSecuencia(){
+        this.secuencia = new Array(10).fill(0).map(n=>Math.floor(Math.random()*4))
+    }
+}
+
+function empezarJuego(){
+     window.juego = new Juego
+}
