e1003392f14c522eb7ef0013d0eafc8344ceb1e6
Marcela Neira <martaymarcela@gmail.com>
2020-12-22 17:50:26 +0100

    Chained promises

--- clase28.js
+++ clase28.js
@@ -18,8 +18,24 @@ function onError(id){
     console.log(`Se produjo un error al obtener el personaje ${id}`)
 }
 
-obtenerPersonaje(1).then(function(data){
+obtenerPersonaje(1).then(data=>{
     console.log(`El personaje 1 es ${data.name}`)
-}).catch(onError)
+    return obtenerPersonaje(2)
+}).then(personaje2=>{
+    console.log(`El personaje 2 es ${personaje2.name}`)
+    return obtenerPersonaje(3)
+}).then(personaje3=>{
+    console.log(`El personaje 3 es ${personaje3.name}`)
+    return obtenerPersonaje(4)
+}).then(personaje4=>{
+    console.log(`El personaje 4 es ${personaje4.name}`)
+    return obtenerPersonaje(5)
+}).then(personaje5=>{
+    console.log(`El personaje 5 es ${personaje5.name}`)
+    return obtenerPersonaje(6)
+}).then(personaje6=>{
+    console.log(`El personaje 6 es ${personaje6.name}`)
+})
+.catch(onError)
     
 
